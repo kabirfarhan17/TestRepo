cbb659d1d9f352928d0b7197dfd8a1d792d31ecc
Farhan <kabirfarhan17@gmail.com>
2021-08-22 17:00:12 -0400

    1st commit

---
+++ src/samplePackage/HelloWorld.java
@@ -0,0 +1,16 @@
+package samplePackage;
+
+public class HelloWorld {
+    static String phrase = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println(phrase);
+
+
+    }
+
+
+
+
+
+}
